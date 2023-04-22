4fb1abab238bc5664ca1c2944423b3f9a5951a8d
BOLOTOVMAX <bolotovmax1983@mail.ru>
2023-04-22 20:51:32 +0300

    commit two

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,58 @@
 function getArrayParams(...arr) {
-
+  let min = Math.min(...arr)
+  let max = Math.max(...arr)
+  let sum = arr.reduce(function(result, current) {
+    return result + current
+  }, );
+  let  avg = sum/arr.length;
+   avg = avg.toFixed(2)
+   avg = Number(avg);
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = arr.reduce(function(result, current) {
+    return result + current
+  }, );
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let differenceMaxMin = max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length;i++){
+    if(arr[i] % 2 === 0){
+      sumEvenElement = arr[i] + sumEvenElement;
+ }else{
+   sumOddElement = arr[i] + sumOddElement;
+ };
+  }
+  let element = sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length;i++){
+     if(arr[i] % 2 === 0){
+       sumEvenElement = arr[i] + sumEvenElement;
+       countEvenElement = countEvenElement + 1;
+     };
+   };
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arr.length;i++){
+   let sumMax = getArrayParams(...arr[i]);
+    if(sumMax > maxWorkerResult){
+      maxWorkerResult = sumMax ;
+    };
+  };
+ return maxWorkerResult
 }
