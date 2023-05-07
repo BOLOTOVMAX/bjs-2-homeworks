49c473a16f24ccce600bb41a56b14571e1e68920
BOLOTOVMAX <bolotovmax1983@mail.ru>
2023-05-07 09:01:23 +0300

    Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,24 +5,19 @@ function Student(name, gender, age) {
     this.marks = []
 }
 
-let student1 = new Student("Василиса", "женский", 19);
-
-let student2 = new Student("Артём", "мужской", 25);
-
-
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName; 
 }
 
 Student.prototype.addMarks = function (...marks) {
    if(this.marks){
-    for(mark of marks)
-    this.marks.push(mark)
+   // for(mark of marks)
+    this.marks.push(...marks)
     }
 }
 
 Student.prototype.getAverage = function () {
-    if (this.marks.length === 0 || this.marks === undefined){
+    if ( this.marks === undefined || this.marks.length === 0){
         return 0;
       }else{
         return (this.marks.reduce((a, b) => a + b, 0))/this.marks.length;
